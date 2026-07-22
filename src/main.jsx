28e0e29da1c58a60c7b23f8b1410f236f74d458b
Yashas <25sjccb173_yashas@sjcc.edu.in>
2026-07-22 07:01:54 +0530

    added vercel analytics and favicon

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,14 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
+import { Analytics } from '@vercel/analytics/react';
 import App from './App';
 import './styles.css';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <App />
+    <>
+      <App />
+      <Analytics />
+    </>
   </React.StrictMode>,
 );
